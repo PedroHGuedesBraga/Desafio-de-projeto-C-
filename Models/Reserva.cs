1827e292be34dba0d0f7508c46ccdade902db22a
Pedro Henrique <pedroguedes58@hotmail.com>
2025-06-19 12:35:38 -0300

    first commit

---
+++ Models/Reserva.cs
@@ -0,0 +1,73 @@
+using System.Runtime.ExceptionServices;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            if (hospedes.Count >= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+                Console.WriteLine("Cadastrado com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Erro");
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // *IMPLEMENTE AQUI*
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+
+            Suite = suite;
+                        
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            int quantidade = Hospedes.Count; 
+            return quantidade;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = 0;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados > 0)
+            {
+
+                valor = DiasReservados * Suite.ValorDiaria;
+
+            }
+            else
+            {
+                Console.WriteLine("erro");
+            }
+            return valor;
+
+            
+        }
+    }
+}
